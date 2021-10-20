8068449bcf189a05fdacee5aed5ace1e90b75d2c
Casey <cantrellcasey@gmail.com>
2021-10-20 16:59:19 -0500

    adds links to appropriate pages in navbar

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@
       <router-link to="/">Home</router-link>
       |
       <router-link to="/about">About</router-link>
+      |
+      <router-link to="/signup">Signup</router-link>
+      |
+      <router-link to="/login">Login</router-link>
+      |
+      <router-link to="/logout">Logout</router-link>
     </div>
     <router-view />
   </div>
@@ -31,3 +37,19 @@
   color: #42b983;
 }
 </style>
+
+<script>
+export default {
+  data: function () {
+    return {};
+  },
+  methods: {
+    isLoggedIn: function () {
+      return localStorage.jwt;
+    },
+    getUserId: function () {
+      return localStorage.user_id;
+    },
+  },
+};
+</script>
