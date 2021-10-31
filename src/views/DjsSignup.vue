f92672bb6bfef946afacd01ebb2145330c50c4b3
Casey <cantrellcasey@gmail.com>
2021-10-31 14:16:49 -0500

    removes unneccessary login code

--- src/views/DjsSignup.vue
+++ src/views/DjsSignup.vue
@@ -55,9 +55,6 @@ export default {
         .post("/djs", this.newDJParams)
         .then((response) => {
           console.log(response.data);
-          axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
-          localStorage.setItem("jwt", response.data.jwt);
-          localStorage.setItem("dj_id", response.data.dj_id);
           window.alert("Account successfully created, please login to continue.");
           this.$router.push("/login");
         })
