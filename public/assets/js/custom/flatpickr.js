bf30a49b89f4f91a01624ab4970a9d9dae914b4b
Casey <cantrellcasey@gmail.com>
2021-11-05 16:11:41 -0500

    fixes buttons and gets ready for theme installation

--- public/assets/js/custom/flatpickr.js
+++
@@ -1,7 +0,0 @@
-let el = document.querySelectorAll("[data-flatpickr]");
-el.forEach(e =>{
-    const t = {
-        ...e.dataset.flatpickr ? JSON.parse(e.dataset.flatpickr) : {},
-    }
-    new flatpickr(e,t)}
-    );
--- public/js/custom/header-sticky.js
+++
@@ -1,9 +0,0 @@
-//Header sticky top
-window.addEventListener('scroll', function () {
-   const headerSticky = document.querySelector(".header-sticky");
-   // var headerStickyFixed = document.querySelector(".header-sticky-fixed");
-   const coords = document.documentElement.clientHeight;
-   if (headerSticky) {
-      headerSticky.classList.toggle("is-sticky", window.scrollY > coords)
-   }
-});
