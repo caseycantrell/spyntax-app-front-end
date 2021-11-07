ba39eb9a9098776392814c6e71153e54b29ad754
Casey <cantrellcasey@gmail.com>
2021-11-07 13:25:34 -0600

    installs bootstrap theme and gets main navbar functionality working

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,56 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <section class="position-relative shadow pb-7 pb-lg-12">
+      <!--Navbar demo heading-->
+
+      <!--Navbar demo start-->
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container position-relative">
+          <a class="navbar-brand" href="index.html">
+            <img src="/assets/img/logo/logo.svg" alt="" class="img-fluid" />
+          </a>
+
+          <div class="d-flex align-items-center navbar-no-collapse-items order-lg-last">
+            <button
+              class="navbar-toggler order-last"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbar-demo-1"
+              aria-controls="navbar-demo-1"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon">
+                <i></i>
+              </span>
+            </button>
+          </div>
+          <div class="collapse navbar-collapse" id="navbar-demo-1">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item me-4">
+                <router-link to="/" class="nav-link">Home</router-link>
+              </li>
+              <li v-if="isLoggedIn()" class="nav-item me-4">
+                <router-link :to="`/requests?dj_id=${getDJId()}`" class="nav-link">Requests</router-link>
+              </li>
+              <li v-if="isLoggedIn()" class="nav-item me-4">
+                <router-link :to="`/djs/${getDJId()}`" class="nav-link">Profile</router-link>
+              </li>
+              <li v-if="!isLoggedIn()" class="nav-item me-4">
+                <router-link to="/djssignup" class="nav-link">Signup</router-link>
+              </li>
+              <li v-if="!isLoggedIn()" class="nav-item me-4">
+                <router-link to="/login" class="nav-link">Login</router-link>
+              </li>
+              <li v-if="isLoggedIn()" class="nav-item me-4">
+                <router-link to="/logout" class="nav-link">Logout</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </section>
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link>
       |
       <router-link v-if="isLoggedIn()" :to="`/requests?dj_id=${getDJId()}`">Requests</router-link>
@@ -12,33 +62,10 @@
       <router-link v-if="!isLoggedIn()" to="/login">Login</router-link>
       |
       <router-link v-if="isLoggedIn()" to="/logout">Logout</router-link>
-    </div>
+    </div> -->
     <router-view />
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
 
 <script>
 export default {
