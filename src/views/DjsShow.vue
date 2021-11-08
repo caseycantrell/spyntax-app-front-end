44250f4f473ddfa7e9fd555c332eeaf4df14093a
Casey <cantrellcasey@gmail.com>
2021-11-08 16:35:28 -0600

    adds new modal logic, unfinished:

--- src/views/DjsShow.vue
+++ src/views/DjsShow.vue
@@ -92,22 +92,49 @@
                         <a href="#!" class="small link-hover-decoration">{{ currentDJ.cashapp }}</a>
                       </li>
                     </ul>
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-                      Display QR Code
-                    </button>
+                    <span class="badge px-2 bg-primary rounded-pill">Display QR Code</span>
                   </div>
                   <div
                     class="modal fade"
-                    id="exampleModalCenter"
+                    id="exampleModal"
                     tabindex="-1"
                     role="dialog"
-                    aria-labelledby="exampleModalCenterTitle"
+                    aria-labelledby="exampleModalLabel"
                     aria-hidden="true"
                   >
-                    <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-dialog" role="document">
                       <div class="modal-content">
                         <div class="modal-header">
-                          <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">...</div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                    Launch demo modal
+                  </button>
+
+                  <!-- Modal -->
+                  <div
+                    class="modal fade"
+                    id="exampleModal"
+                    tabindex="-1"
+                    role="dialog"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                  >
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                           </button>
@@ -197,9 +224,9 @@ export default {
     getDJId: function () {
       return localStorage.dj_id;
     },
-    // showQRCode: function () {
-    //   document.querySelector("#qr-code").showModal();
-    // },
+    showQRCode: function () {
+      document.querySelector("#qr-code").showModal();
+    },
     doPrint: function () {
       window.print();
     },
