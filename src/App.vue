dfa6740960512ea8f4edd5a295d806f25ad2deed
Casey <cantrellcasey@gmail.com>
2021-11-11 11:34:19 -0600

    changes logo

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container position-relative">
           <a class="navbar-brand" href="/">
-            <img src="/assets/img/spyntax1.png" width="180px" alt="" />
+            <img src="/assets/img/spyntax2.png" width="180px" alt="" />
           </a>
 
           <div class="d-flex align-items-center navbar-no-collapse-items order-lg-last">
